de09174acd724737aa4a9e50c2f5d9582f28b8e7
KolAnastasia <53018954+KolAnastasia@users.noreply.github.com>
2020-06-23 18:02:00 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $('.banner_slider').slick({
-    
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.banner-wrap_nav_prev'),
+    nextArrow: $('.banner-wrap_nav_next'),    
+});
+
+$('.main-advantages_slider').slick({
+    dots: false,
+    prevArrow: $('.main-advantages_prev'),
+    nextArrow: $('.main-advantages_next'),    
+   
+})
+
+$(".main-advantages_slider").on('afterChange', function(event, slick, currentSlide){
+    $("#cp").text(currentSlide + 1);
+ });
+
+ $('.menu_item_icon').click(function() {
+    $('.menu_item_icon').toggleClass('active');
+    $('.nav_menu').toggleClass('active');
 });
