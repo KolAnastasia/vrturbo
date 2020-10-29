0b6ac8222ec35bebbcce4e51b18473069231f0fe
KolAnastasia <53018954+KolAnastasia@users.noreply.github.com>
2020-10-29 11:48:36 +0300

    Initial commit

--- js/script.js
+++ js/script.js
@@ -3,21 +3,34 @@ $('.banner_slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: $('.banner-wrap_nav_prev'),
-    nextArrow: $('.banner-wrap_nav_next'),    
+    nextArrow: $('.banner-wrap_nav_next'),
+    appendDots: $('.banner_slider_nav-wr'),
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+
 });
 
 $('.main-advantages_slider').slick({
     dots: false,
     prevArrow: $('.main-advantages_prev'),
-    nextArrow: $('.main-advantages_next'),    
-   
+    nextArrow: $('.main-advantages_next'),
+
 })
 
-$(".main-advantages_slider").on('afterChange', function(event, slick, currentSlide){
-    $("#cp").text(currentSlide + 1);
- });
+$(".main-advantages_slider").on('afterChange', function (event, slick, currentSlide) {
+    var num = currentSlide + 1;
+    var strNum = String(num);
+    var zNum = '0'+ strNum;
+    $("#cp").text(zNum);
+});
 
- $('.menu_item_icon').click(function() {
+$('.menu_item_icon').click(function () {
     $('.menu_item_icon').toggleClass('active');
     $('.nav_menu').toggleClass('active');
 });
