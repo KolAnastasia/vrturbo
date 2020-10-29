eeed8f902c7f2f3cd3ae57ca0df26d681d3d6952
KolAnastasia <53018954+KolAnastasia@users.noreply.github.com>
2020-10-29 15:44:57 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import 'jquery'
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'slick-carousel'
+import './script.js';
+import '../../node_modules/air-datepicker/dist/js/datepicker';
+
+const fancybox = require("@fancyapps/fancybox");
